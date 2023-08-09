92aee000b86f3421b931181078dfc5e547cdd9cd
[akif ödül] <[akifodul@hotmail.com]>
2023-08-09 10:36:56 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int yil;
+        Scanner input = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Yıl Giriniz: ");
+        yil = input.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if (yil % 4 == 0 && yil % 100 != 0 || yil % 400 == 0) {
+            System.out.print(yil + " bir artık yıldır!");
+
+        } else {
+            System.out.print(yil + " bir artık yıl değildir!");
         }
     }
 }
